2751efbc1294cee997764d2a5875a6b2adefebf3
ticinovicsara <ticinovicsara0@gmail.com>
2025-02-18 14:34:00 +0100

    reset filters

--- CarDealership/src/components/carList.jsx
+++ CarDealership/src/components/carList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import "../styles/car-list.css";
+import "../styles/car-list-filter.css";
 
 const CarList = ({ cars, removeCar }) => {
   const containerRef = useRef(null);
@@ -38,17 +39,20 @@ const CarList = ({ cars, removeCar }) => {
     return a.model.localeCompare(b.model);
   });
 
-  const applyFilter = () => {
-    let newFilteredCars = sortedCars.filter(
+  useEffect(() => {
+    const newFilteredCars = cars.filter(
       (car) =>
         (filterBrand === "" ||
           car.brand.toLowerCase().includes(filterBrand.toLowerCase())) &&
         (filterModel === "" ||
           car.model.toLowerCase().includes(filterModel.toLowerCase()))
     );
-
     setFilteredCars(newFilteredCars);
-    setShowFilters(false);
+  }, [filterBrand, filterModel, cars]);
+
+  const resetFilters = () => {
+    setFilterBrand("");
+    setFilterModel("");
   };
 
   const isRegistrationExpired = (dateString) => {
@@ -69,6 +73,12 @@ const CarList = ({ cars, removeCar }) => {
       {showFilters && (
         <div className="filter-popup">
           <div className="filter-popup-content">
+            <button
+              className="close-filter-btn"
+              onClick={() => setShowFilters(false)}
+            >
+              𝘅
+            </button>
             <h3>Filter Vehicles</h3>
             <input
               type="text"
@@ -82,49 +92,50 @@ const CarList = ({ cars, removeCar }) => {
               value={filterModel}
               onChange={(e) => setFilterModel(e.target.value)}
             />
-            <button className="apply-filter-btn" onClick={applyFilter}>
+            {/* <button className="apply-filter-btn" onClick={applyFilter}>
               Apply Filter
-            </button>
-            <button
-              className="close-filter-btn"
-              onClick={() => setShowFilters(false)}
-            >
-              Close
+            </button> */}
+            <button className="reset-filter-btn" onClick={resetFilters}>
+              {" "}
+              Reset filters
             </button>
           </div>
         </div>
       )}
 
-      <motion.div
-        className="car-list"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-      >
-        {filteredCars.map((car, index) => (
-          <motion.div
-            key={car.id}
-            className={`car-card ${
-              isRegistrationExpired(car.registration) ? "expired" : ""
-            }`}
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: index * 0.1 }}
-          >
-            <h3 className="card-title">
-              {car.brand} {car.model}
-            </h3>
-            <p className="card-content">Type: {car.type}</p>
-            <p className="card-content">Year: {car.year}</p>
-            <p className="card-content">Registration: {car.registration}</p>
-            <button className="delete-btn" onClick={() => removeCar(car.id)}>
-              Remove
-            </button>
-          </motion.div>
-        ))}
-      </motion.div>
+      {filteredCars.length === 0 ? (
+        <div className="no-cars">No vehicles match the selected filters.</div>
+      ) : (
+        <motion.div
+          className="car-list"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+        >
+          {filteredCars.map((car, index) => (
+            <motion.div
+              key={car.id}
+              className={`car-card ${
+                isRegistrationExpired(car.registration) ? "expired" : ""
+              }`}
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.1 }}
+            >
+              <h3 className="card-title">
+                {car.brand} {car.model}
+              </h3>
+              <p className="card-content">Type: {car.type}</p>
+              <p className="card-content">Year: {car.year}</p>
+              <p className="card-content">Registration: {car.registration}</p>
+              <button className="delete-btn" onClick={() => removeCar(car.id)}>
+                Remove
+              </button>
+            </motion.div>
+          ))}
+        </motion.div>
+      )}
     </div>
   );
 };
-
 export default CarList;
